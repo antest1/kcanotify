14598fe8002fed58767f5c5155defe344ab111d5
antest1 <alias_n@live.jp>
2017-02-08 01:22:11 +0900

    add features for main service / BattleView button
    - add hide action for BattleView button (Show Again/Remove)
    - show "swipe down for see content" for view notification
    - check heavy damaged state for selected fleet when sortie start
    - fix heavy damage state check logic (for combined fleet)

--- app/src/main/java/com/antest1/kcanotify/KcaViewButtonService.java
+++ app/src/main/java/com/antest1/kcanotify/KcaViewButtonService.java
@@ -1,16 +1,22 @@
 package com.antest1.kcanotify;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.BitmapFactory;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.graphics.PorterDuff;
-import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Vibrator;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
@@ -21,7 +27,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.gson.JsonObject;
@@ -37,6 +42,10 @@ import static com.antest1.kcanotify.KcaConstants.KCA_MSG_BATTLE_VIEW_REFRESH;
 import static com.antest1.kcanotify.KcaConstants.KCA_MSG_DATA;
 
 public class KcaViewButtonService extends Service {
+    public static final int FAIRY_NOTIFICATION_ID = 10118;
+    public static final String RETURN_FAIRY_ACTION = "return_fairy_action";
+    public static final String REMOVE_FAIRY_ACTION = "remove_fairy_action";
+
     private LocalBroadcastManager broadcaster;
     private BroadcastReceiver battleinfo_receiver;
     private BroadcastReceiver battlehdmg_receiver;
@@ -44,11 +53,11 @@ public class KcaViewButtonService extends Service {
     private BroadcastReceiver buttontop_receiver;
     private View mView;
     private WindowManager mManager;
-    private boolean isViewEnabled;
-
+    private Handler mHandler;
+    private Vibrator vibrator;
     private ImageView viewbutton;
     WindowManager.LayoutParams mParams;
-
+    NotificationManagerCompat notificationManager;
     public static JsonObject currentApiData;
     public static int type;
     public static int clickcount;
@@ -61,6 +70,10 @@ public class KcaViewButtonService extends Service {
         return clickcount;
     }
 
+    public String getStringWithLocale(int id) {
+        return KcaUtils.getStringWithLocale(getApplicationContext(), getBaseContext(), id);
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -71,7 +84,7 @@ public class KcaViewButtonService extends Service {
     public void onCreate() {
         super.onCreate();
         clickcount = 0;
-        isViewEnabled = false;
+        mHandler = new Handler();
         startService(new Intent(getBaseContext(), KcaBattleViewService.class));
         broadcaster = LocalBroadcastManager.getInstance(this);
         battleinfo_receiver = new BroadcastReceiver() {
@@ -115,6 +128,7 @@ public class KcaViewButtonService extends Service {
         LocalBroadcastManager.getInstance(this).registerReceiver((battlenode_receiver), new IntentFilter(KCA_MSG_BATTLE_NODE));
         LocalBroadcastManager.getInstance(this).registerReceiver((battlehdmg_receiver), new IntentFilter(KCA_MSG_BATTLE_HDMG));
         LayoutInflater mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        notificationManager = NotificationManagerCompat.from(getApplicationContext());
         mView = mInflater.inflate(R.layout.view_button, null);
         viewbutton = (ImageView) mView.findViewById(R.id.viewbutton);
         viewbutton.getDrawable().clearColorFilter();
@@ -127,7 +141,7 @@ public class KcaViewButtonService extends Service {
                 PixelFormat.TRANSLUCENT);
 
         mParams.gravity = Gravity.TOP | Gravity.LEFT;
-
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         Display display = ((WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
         int width, height;
         Point size = new Point();
@@ -142,6 +156,16 @@ public class KcaViewButtonService extends Service {
         mParams.y = height - vh;
         mManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         mManager.addView(mView, mParams);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if(intent.getAction() != null && intent.getAction().equals(RETURN_FAIRY_ACTION)) {
+            mView.setVisibility(View.VISIBLE);
+        }
+        notificationManager.cancel(FAIRY_NOTIFICATION_ID);
+        Log.e("KCA-V", "onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
@@ -160,8 +184,11 @@ public class KcaViewButtonService extends Service {
 
     private View.OnTouchListener mViewTouchListener = new View.OnTouchListener() {
         private static final int MAX_CLICK_DURATION = 200;
-        private long startClickTime;
+        private static final int LONG_CLICK_DURATION = 800;
 
+        private long startClickTime;
+        private long recentClickTime;
+        private boolean longPressTriggered = false;
         @Override
         public boolean onTouch(View v, MotionEvent event) {
             switch (event.getAction()) {
@@ -171,9 +198,12 @@ public class KcaViewButtonService extends Service {
                     mViewX = mParams.x;
                     mViewY = mParams.y;
                     startClickTime = Calendar.getInstance().getTimeInMillis();
+                    mHandler.postDelayed(mRunnable, LONG_CLICK_DURATION);
                     break;
 
                 case MotionEvent.ACTION_UP:
+                    Log.e("KCA", "Callback Canceled");
+                    mHandler.removeCallbacks(mRunnable);
                     long clickDuration = Calendar.getInstance().getTimeInMillis() - startClickTime;
                     if (clickDuration < MAX_CLICK_DURATION) {
                         clickcount += 1;
@@ -184,6 +214,7 @@ public class KcaViewButtonService extends Service {
                     int xx = locations[0];
                     int yy = locations[1];
                     Log.e("KCA", String.format("Coord: %d %d", xx, yy));
+                    longPressTriggered = false;
                     break;
 
                 case MotionEvent.ACTION_MOVE:
@@ -193,9 +224,54 @@ public class KcaViewButtonService extends Service {
                     mParams.x = mViewX + x;
                     mParams.y = mViewY + y;
                     mManager.updateViewLayout(mView, mParams);
+                    if(Math.abs(x) > 20 || Math.abs(y) > 20) {
+                        Log.e("KCA", "Callback Canceled");
+                        mHandler.removeCallbacks(mRunnable);
+                    }
                     break;
             }
             return true;
         }
     };
+
+
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            vibrator.vibrate(100);
+            Toast.makeText(getApplicationContext(), getStringWithLocale(R.string.viewbutton_hide), Toast.LENGTH_LONG).show();
+            mView.setVisibility(View.GONE);
+            displayNotification(getApplicationContext());
+        }
+    };
+
+    public void displayNotification(Context context) {
+
+        Intent returnIntent = new Intent(this, KcaViewButtonService.class)
+                .setAction(RETURN_FAIRY_ACTION);
+
+        Intent removeIntent = new Intent(this, KcaViewButtonService.class)
+                .setAction(REMOVE_FAIRY_ACTION);
+
+        PendingIntent returnPendingIntent = PendingIntent.getService(context, 0,
+                returnIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        PendingIntent removePendingIntent = PendingIntent.getService(context, 0,
+                removeIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.mipmap.noti_icon4)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.noti_icon3))
+                        .setContentTitle(getStringWithLocale(R.string.fairy_hidden_notification_title))
+                        .setContentText(getStringWithLocale(R.string.fairy_hidden_notification_text))
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(getStringWithLocale(R.string.fairy_hidden_notification_bigtext)))
+                        .setPriority(Notification.PRIORITY_MAX)
+                        .setOngoing(true)
+                        .addAction(new NotificationCompat.Action(R.mipmap.noti_icon4,
+                                getStringWithLocale(R.string.fairy_hidden_notification_action_return), returnPendingIntent))
+                        .addAction(new NotificationCompat.Action(R.mipmap.ic_cancel,
+                                getStringWithLocale(R.string.fairy_hidden_notification_action_remove), removePendingIntent));
+
+        notificationManager.notify(FAIRY_NOTIFICATION_ID, notificationBuilder.build());
+    }
 }
