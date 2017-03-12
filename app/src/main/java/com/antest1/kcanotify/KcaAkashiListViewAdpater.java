7b414e11ea719a38fc3c17f6705b2df7d4cd87b8
antest1 <alias_n@live.jp>
2017-03-12 19:31:13 +0900

    fix bug for listview (return null in getView)

--- app/src/main/java/com/antest1/kcanotify/KcaAkashiListViewAdpater.java
+++ app/src/main/java/com/antest1/kcanotify/KcaAkashiListViewAdpater.java
@@ -55,65 +55,62 @@ public class KcaAkashiListViewAdpater extends BaseAdapter {
         // format: |1|23|55|260|
         String starlistData = getStringPreferences(context, PREF_AKASHI_STARLIST);
         if (convertView == null) {
-
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.listview_equip_item, parent, false);
-
-            LinearLayout itemView = (LinearLayout) convertView.findViewById(R.id.akashi_improv_item_area);
-            ImageView iconView = (ImageView) convertView.findViewById(R.id.akashi_improv_icon);
-            TextView nameView = (TextView) convertView.findViewById(R.id.akashi_improv_name);
-            TextView screwView = (TextView) convertView.findViewById(R.id.akashi_improv_screws);
-            TextView supportView = (TextView) convertView.findViewById(R.id.akashi_improv_support);
-            TextView starView = (TextView) convertView.findViewById(R.id.akashi_improv_star);
-
-            KcaAkashiListViewItem item = listViewItemList.get(position);
-            final int itemId = item.getEquipId();
-            final String itemImprovmentData = item.getEquipImprovmentData().toString();
-
-            iconView.setImageResource(item.getEquipIconMipmap());
-            nameView.setText(item.getEquipName());
-            screwView.setText(item.getEquipScrews());
-            supportView.setText(item.getEquipSupport());
-            screwView.setTextColor(ContextCompat.getColor(context, getScrewTextColor(isSafeChecked)));
-            if (checkStarred(starlistData, itemId)) {
-                starView.setText(context.getString(R.string.aa_btn_star1));
-            } else {
-                starView.setText(context.getString(R.string.aa_btn_star0));
-            }
-
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(context, AkashiDetailActivity.class);
-                    intent.putExtra("item_id", itemId);
-                    intent.putExtra("item_info", itemImprovmentData);
-                    context.startActivity(intent);
-                }
-            });
-
-            starView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String starred = getStringPreferences(context, PREF_AKASHI_STARLIST);
-                    TextView tv = (TextView) v;
-                    if(tv.getText().equals(context.getString(R.string.aa_btn_star0))) {
-                        setPreferences(context, PREF_AKASHI_STARLIST,
-                                addStarred(starred, itemId));
-                        tv.setText(context.getString(R.string.aa_btn_star1));
-                    } else {
-                        setPreferences(context, PREF_AKASHI_STARLIST,
-                                deleteStarred(starred, itemId));
-                        tv.setText(context.getString(R.string.aa_btn_star0));
-                    }
-                    Log.e("KCA", starred);
-                    sHandler.sendEmptyMessage(0);
-                }
-            });
-
-            return convertView;
         }
 
-        return null;
+        LinearLayout itemView = (LinearLayout) convertView.findViewById(R.id.akashi_improv_item_area);
+        ImageView iconView = (ImageView) convertView.findViewById(R.id.akashi_improv_icon);
+        TextView nameView = (TextView) convertView.findViewById(R.id.akashi_improv_name);
+        TextView screwView = (TextView) convertView.findViewById(R.id.akashi_improv_screws);
+        TextView supportView = (TextView) convertView.findViewById(R.id.akashi_improv_support);
+        TextView starView = (TextView) convertView.findViewById(R.id.akashi_improv_star);
+
+        KcaAkashiListViewItem item = listViewItemList.get(position);
+        final int itemId = item.getEquipId();
+        final String itemImprovmentData = item.getEquipImprovmentData().toString();
+
+        iconView.setImageResource(item.getEquipIconMipmap());
+        nameView.setText(item.getEquipName());
+        screwView.setText(item.getEquipScrews());
+        supportView.setText(item.getEquipSupport());
+        screwView.setTextColor(ContextCompat.getColor(context, getScrewTextColor(isSafeChecked)));
+        if (checkStarred(starlistData, itemId)) {
+            starView.setText(context.getString(R.string.aa_btn_star1));
+        } else {
+            starView.setText(context.getString(R.string.aa_btn_star0));
+        }
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, AkashiDetailActivity.class);
+                intent.putExtra("item_id", itemId);
+                intent.putExtra("item_info", itemImprovmentData);
+                context.startActivity(intent);
+            }
+        });
+
+        starView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String starred = getStringPreferences(context, PREF_AKASHI_STARLIST);
+                TextView tv = (TextView) v;
+                if (tv.getText().equals(context.getString(R.string.aa_btn_star0))) {
+                    setPreferences(context, PREF_AKASHI_STARLIST,
+                            addStarred(starred, itemId));
+                    tv.setText(context.getString(R.string.aa_btn_star1));
+                } else {
+                    setPreferences(context, PREF_AKASHI_STARLIST,
+                            deleteStarred(starred, itemId));
+                    tv.setText(context.getString(R.string.aa_btn_star0));
+                }
+                Log.e("KCA", starred);
+                sHandler.sendEmptyMessage(0);
+            }
+        });
+
+        return convertView;
     }
 
     public void setListViewItemList(ArrayList<KcaAkashiListViewItem> list) {
@@ -130,7 +127,7 @@ public class KcaAkashiListViewAdpater extends BaseAdapter {
     }
 
     private boolean checkStarred(String data, int id) {
-        return data.contains(String.format("|%d|",id));
+        return data.contains(String.format("|%d|", id));
     }
 
     private String addStarred(String data, int id) {
@@ -138,6 +135,6 @@ public class KcaAkashiListViewAdpater extends BaseAdapter {
     }
 
     private String deleteStarred(String data, int id) {
-        return data.replace(String.format("|%d|",id), "|");
+        return data.replace(String.format("|%d|", id), "|");
     }
 }
