c26bfd23f6137fd4b0cf6acadb722b1b74144e47
antest1 <alias_n@live.jp>
2017-11-14 20:49:44 +0900

    not process when key not passed

--- app/src/main/java/com/antest1/kcanotify/KcaInspectorDetailActivity.java
+++ app/src/main/java/com/antest1/kcanotify/KcaInspectorDetailActivity.java
@@ -60,96 +60,94 @@ public class KcaInspectorDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inspector_detail);
-
         Bundle bundle = getIntent().getExtras();
-        String type_key = bundle.getString("key");
+        String type_key = bundle.getString("key", null);
+        if (type_key == null) {
+            finish();
+        } else {
+            setContentView(R.layout.activity_inspector_detail);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(type_key);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle(type_key);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        dbHelper = new KcaDBHelper(getApplicationContext(), null, KCANOTIFY_DB_VERSION);
-        questTracker = new KcaQuestTracker(getApplicationContext(), null, KCANOTIFY_QTDB_VERSION);
+            dbHelper = new KcaDBHelper(getApplicationContext(), null, KCANOTIFY_DB_VERSION);
+            questTracker = new KcaQuestTracker(getApplicationContext(), null, KCANOTIFY_QTDB_VERSION);
 
-        sv = findViewById(R.id.inspect_data_scrollview);
-        sv.setSmoothScrollingEnabled(false);
+            sv = findViewById(R.id.inspect_data_scrollview);
+            sv.setSmoothScrollingEnabled(false);
 
-        view_holder = findViewById(R.id.inspect_data_view);
-        view_key = findViewById(R.id.inspect_data_key);
-        view_value = findViewById(R.id.inspect_data_value);
-        view_format = findViewById(R.id.inspect_data_format);
+            view_holder = findViewById(R.id.inspect_data_view);
+            view_key = findViewById(R.id.inspect_data_key);
+            view_value = findViewById(R.id.inspect_data_value);
+            view_format = findViewById(R.id.inspect_data_format);
 
-        view_format.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (value_text == null || value_text.length() > 0) {
-                    is_formatted = !is_formatted;
-                    setText();
-                }
-            }
-        });
-
-        String[] type_key_list = type_key.split(" ");
-        key = type_key_list[1];
-        view_key.setText(key);
-
-        if (type_key.startsWith(DB_PREFIX)) {
-            value_text = dbHelper.getValue(key);
-            if (value_text == null) value_text = "<null>";
-        } else if (type_key.startsWith(PREF_PREFIX)) {
-            if (PREFS_BOOLEAN_LIST.contains(key)) {
-                value_text = String.valueOf(getBooleanPreferences(getApplicationContext(), key));
-            } else {
-                value_text = getStringPreferences(getApplicationContext(), key);
-            }
-        } else if (type_key.startsWith(QT_PREFIX)) {
-            value_text = questTracker.getQuestTrackerData();
-        } else if (type_key.startsWith(DQ_PREFIX)) {
-            value_text = dbHelper.getQuestListData();
-        }
-        setText();
-        sv.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
-            @Override
-            public void onScrollChanged() {
-                int s_y = sv.getScrollY();
-            }
-        });
-
-        sv.post(new Runnable() {
-            @Override
-            public void run() {
-                sv.measure(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                scroll_h_total = sv.getMeasuredHeight();
-                scroll_h_layout = sv.getHeight();
-
-                sv.setOnTouchListener(new View.OnTouchListener() {
-                    @Override
-                    public boolean onTouch(View view, MotionEvent motionEvent) {
-                        switch (motionEvent.getAction()) {
-                            case MotionEvent.ACTION_DOWN:
-                                int y = (int) motionEvent.getY();
-                                int direction = 1;
-                                if (y < scroll_h_layout / 2) direction = -1;
-                                scroll_touch_count = 0;
-                                autoScrollScheduler = Executors.newSingleThreadScheduledExecutor();
-                                autoScrollScheduler.scheduleAtFixedRate(auto_scroll(direction), 800, 200, TimeUnit.MILLISECONDS);
-                                break;
-                            case MotionEvent.ACTION_UP:
-                                autoScrollScheduler.shutdown();
-                                break;
-                        }
-                        return false;
+            view_format.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (value_text == null || value_text.length() > 0) {
+                        is_formatted = !is_formatted;
+                        setText();
                     }
-                });
-            }
-        });
-    }
+                }
+            });
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+            String[] type_key_list = type_key.split(" ");
+            key = type_key_list[1];
+            view_key.setText(key);
+
+            if (type_key.startsWith(DB_PREFIX)) {
+                value_text = dbHelper.getValue(key);
+                if (value_text == null) value_text = "<null>";
+            } else if (type_key.startsWith(PREF_PREFIX)) {
+                if (PREFS_BOOLEAN_LIST.contains(key)) {
+                    value_text = String.valueOf(getBooleanPreferences(getApplicationContext(), key));
+                } else {
+                    value_text = getStringPreferences(getApplicationContext(), key);
+                }
+            } else if (type_key.startsWith(QT_PREFIX)) {
+                value_text = questTracker.getQuestTrackerData();
+            } else if (type_key.startsWith(DQ_PREFIX)) {
+                value_text = dbHelper.getQuestListData();
+            }
+            setText();
+            sv.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+                @Override
+                public void onScrollChanged() {
+                    int s_y = sv.getScrollY();
+                }
+            });
+
+            sv.post(new Runnable() {
+                @Override
+                public void run() {
+                    sv.measure(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    scroll_h_total = sv.getMeasuredHeight();
+                    scroll_h_layout = sv.getHeight();
+
+                    sv.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View view, MotionEvent motionEvent) {
+                            switch (motionEvent.getAction()) {
+                                case MotionEvent.ACTION_DOWN:
+                                    int y = (int) motionEvent.getY();
+                                    int direction = 1;
+                                    if (y < scroll_h_layout / 2) direction = -1;
+                                    scroll_touch_count = 0;
+                                    autoScrollScheduler = Executors.newSingleThreadScheduledExecutor();
+                                    autoScrollScheduler.scheduleAtFixedRate(auto_scroll(direction), 800, 200, TimeUnit.MILLISECONDS);
+                                    break;
+                                case MotionEvent.ACTION_UP:
+                                    autoScrollScheduler.shutdown();
+                                    break;
+                            }
+                            return false;
+                        }
+                    });
+                }
+            });
+        }
     }
 
     public void setText() {
