cb30c74a064f05cc1089e8e7eaf131ed30fa2852
antest1 <alias_n@live.jp>
2018-01-15 01:10:43 +0900

    fix reward resource and condition in A1~A3, B1~B2, activate B2

--- app/src/main/java/com/antest1/kcanotify/KcaExpeditionCheckViewService.java
+++ app/src/main/java/com/antest1/kcanotify/KcaExpeditionCheckViewService.java
@@ -51,7 +51,7 @@ public class KcaExpeditionCheckViewService extends Service {
     public static final String SHOW_EXCHECKVIEW_ACTION = "show_excheckview";
     final int[] expedition_list = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
             21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 35, 36, 37, 38, 39, 40,
-            100, 101, 102, 110, 133, 134};
+            100, 101, 102, 110, 111, 133, 134};
     public static final double[][] toku_bonus = {
             {2.0, 2.0, 2.0, 2.0, 2.0},
             {4.0, 4.0, 4.0, 4.0, 4.0},
