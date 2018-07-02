d4f84de000c1957d0d107d09d6657eb67aa005d9
antest1 <alias_n@live.jp>
2018-07-02 22:42:48 +0900

    remove debug condition

--- app/src/main/java/com/antest1/kcanotify/MainActivity.java
+++ app/src/main/java/com/antest1/kcanotify/MainActivity.java
@@ -172,7 +172,7 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 Intent intent = new Intent(MainActivity.this, KcaService.class);
                 if (isChecked) {
-                    if (true || is_kca_installed) {
+                    if (is_kca_installed) {
                         if (!prefs.getBoolean(PREF_SVC_ENABLED, false)) {
                             loadTranslationData(getApplicationContext());
                             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
