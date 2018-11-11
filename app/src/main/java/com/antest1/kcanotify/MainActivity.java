fe3b1970d11d24bffaf5e71dbff0284c74e9f059
antest1 <alias_n@live.jp>
2018-11-11 18:21:54 +0900

    remove restriction for kca installation

--- app/src/main/java/com/antest1/kcanotify/MainActivity.java
+++ app/src/main/java/com/antest1/kcanotify/MainActivity.java
@@ -171,13 +171,9 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 Intent intent = new Intent(MainActivity.this, KcaService.class);
                 if (isChecked) {
-                    if (is_kca_installed) {
-                        if (!prefs.getBoolean(PREF_SVC_ENABLED, false)) {
-                            loadTranslationData(getApplicationContext());
-                            startService(intent);
-                        }
-                    } else {
-                        Toast.makeText(getApplicationContext(), getString(R.string.ma_toast_kancolle_not_installed), Toast.LENGTH_LONG).show();
+                    if (!prefs.getBoolean(PREF_SVC_ENABLED, false)) {
+                        loadTranslationData(getApplicationContext());
+                        startService(intent);
                     }
                 } else {
                     stopService(intent);
