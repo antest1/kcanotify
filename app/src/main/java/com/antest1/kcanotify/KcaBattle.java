a7a1381f7a0589445fd2f3ef8ac48198831fe0f0
antest1 <alias_n@live.jp>
2019-06-04 23:08:31 +0900

    handle friendly battle for other night battles

--- app/src/main/java/com/antest1/kcanotify/KcaBattle.java
+++ app/src/main/java/com/antest1/kcanotify/KcaBattle.java
@@ -970,6 +970,14 @@ public class KcaBattle {
                     calculateSupportDamage(support_info);
                 }
 
+                if (isKeyExist(api_data, "api_friendly_battle")) {
+                    JsonObject friend_battle = api_data.getAsJsonObject("api_friendly_battle");
+                    JsonObject friend_hougeki = friend_battle.getAsJsonObject("api_hougeki");
+                    if (isKeyExist(friend_hougeki, "api_df_list")) {
+                        calculateFriendSupportFleetHougekiDamage(friend_hougeki);
+                    }
+                }
+
                 if (isKeyExist(api_data, "api_hougeki")) {
                     JsonObject hougeki = api_data.getAsJsonObject("api_hougeki");
                     if (isKeyExist(hougeki, "api_df_list")) {
@@ -1581,6 +1589,14 @@ public class KcaBattle {
                 if (isKeyExist(api_data, "api_n_support_info")) {
                     JsonObject support_info = api_data.getAsJsonObject("api_n_support_info");
                     calculateSupportDamage(support_info);
+                }
+
+                if (isKeyExist(api_data, "api_friendly_battle")) {
+                    JsonObject friend_battle = api_data.getAsJsonObject("api_friendly_battle");
+                    JsonObject friend_hougeki = friend_battle.getAsJsonObject("api_hougeki");
+                    if (isKeyExist(friend_hougeki, "api_df_list")) {
+                        calculateFriendSupportFleetHougekiDamage(friend_hougeki);
+                    }
                 }
 
                 if (isKeyExist(api_data, "api_hougeki")) {
