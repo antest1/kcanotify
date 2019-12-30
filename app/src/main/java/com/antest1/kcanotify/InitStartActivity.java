15d3c57fb214c2633ab86908408a3217978d1f1f
antest1 <alias_n@live.jp>
2019-12-30 13:30:40 +0900

    fix splash page

--- app/src/main/java/com/antest1/kcanotify/InitStartActivity.java
+++ app/src/main/java/com/antest1/kcanotify/InitStartActivity.java
@@ -2,26 +2,18 @@ package com.antest1.kcanotify;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.arch.persistence.room.Update;
 import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.AssetManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.PowerManager;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,13 +25,6 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
-import com.tonyodev.fetch2.Download;
-import com.tonyodev.fetch2.Fetch;
-import com.tonyodev.fetch2.FetchConfiguration;
-import com.tonyodev.fetch2.FetchListener;
-import com.tonyodev.fetch2.Request;
-
-import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -49,17 +34,13 @@ import java.io.OutputStream;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.zip.GZIPInputStream;
 
 import retrofit2.Call;
-import retrofit2.Callback;
 
 import static com.antest1.kcanotify.KcaApiData.loadTranslationData;
 import static com.antest1.kcanotify.KcaConstants.DB_KEY_KCMAINTNC;
 import static com.antest1.kcanotify.KcaConstants.DB_KEY_STARTDATA;
-import static com.antest1.kcanotify.KcaConstants.ERROR_TYPE_DATALOAD;
 import static com.antest1.kcanotify.KcaConstants.ERROR_TYPE_MAIN;
-import static com.antest1.kcanotify.KcaConstants.ERROR_TYPE_SETTING;
 import static com.antest1.kcanotify.KcaConstants.KCANOTIFY_DB_VERSION;
 import static com.antest1.kcanotify.KcaConstants.PREFS_LIST;
 import static com.antest1.kcanotify.KcaConstants.PREF_ALLOW_EXTFILTER;
@@ -67,7 +48,6 @@ import static com.antest1.kcanotify.KcaConstants.PREF_APK_DOWNLOAD_SITE;
 import static com.antest1.kcanotify.KcaConstants.PREF_CHECK_UPDATE_START;
 import static com.antest1.kcanotify.KcaConstants.PREF_DATALOAD_ERROR_FLAG;
 import static com.antest1.kcanotify.KcaConstants.PREF_FAIRY_ICON;
-import static com.antest1.kcanotify.KcaConstants.PREF_KCARESOURCE_VERSION;
 import static com.antest1.kcanotify.KcaConstants.PREF_KCA_DATA_VERSION;
 import static com.antest1.kcanotify.KcaConstants.PREF_KCA_VERSION;
 import static com.antest1.kcanotify.KcaConstants.PREF_LAST_UPDATE_CHECK;
@@ -84,37 +64,21 @@ import static com.antest1.kcanotify.KcaUtils.sendUserAnalytics;
 import static com.antest1.kcanotify.KcaUtils.setPreferences;
 
 public class InitStartActivity extends Activity {
-    public final static String FAIRY_INFO_FILENAME = "icon_info.json";
-    public static final int UPDATECHECK_INTERVAL_MS = 30000;
     public static final String ACTION_RESET = "ACTION_RESET";
-
-    public final static String DOWNLOAD_TYPE_APPDATA = "appdata";
-    public final static String DOWNLOAD_TYPE_GAMEDATA = "gamedata";
-    public final static String DOWNLOAD_TYPE_QUESTINFO = "questinfo";
-    public final static String DOWNLOAD_TYPE_SHIPINFO = "shipinfo";
-    public final static String DOWNLOAD_TYPE_EQUIPINFO = "equipinfo";
-    public final static String DOWNLOAD_TYPE_AKASHI = "akashi";
-    public final static String DOWNLOAD_TYPE_FAIRY = "fairy";
-    public final static String[] DOWNLOAD_TYPE_LIST = {
-            DOWNLOAD_TYPE_APPDATA, DOWNLOAD_TYPE_GAMEDATA, DOWNLOAD_TYPE_QUESTINFO,
-            DOWNLOAD_TYPE_SHIPINFO, DOWNLOAD_TYPE_EQUIPINFO, DOWNLOAD_TYPE_AKASHI,
-            DOWNLOAD_TYPE_FAIRY
-    };
 
     boolean download_finished = false;
     Handler handler = new Handler();
 
     KcaDBHelper dbHelper;
     KcaDownloader downloader;
-    ProgressDialog mProgressDialog;
     TextView appname, appversion, appmessage;
     ImageView appfront;
 
     boolean is_destroyed = false;
     int fairy_flag, new_resversion;
     boolean reset_flag = false;
+    boolean validated_flag = false;
     boolean is_skipped = false;
-    Fetch fetch;
 
     static Gson gson = new Gson();
     Type listType = new TypeToken<ArrayList<JsonObject>>(){}.getType();
@@ -133,13 +97,6 @@ public class InitStartActivity extends Activity {
 
         Intent mainIntent = getIntent();
         reset_flag = mainIntent.getBooleanExtra(ACTION_RESET, false);
-        // instantiate it within the onCreate method
-        mProgressDialog = new ProgressDialog(InitStartActivity.this);
-        mProgressDialog.setMessage(getStringWithLocale(R.string.download_progress));
-        mProgressDialog.setIndeterminate(true);
-        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        mProgressDialog.setCancelable(false);
-        mProgressDialog.setProgressNumberFormat("%1d file(s)");
         is_destroyed = false;
 
         PreferenceManager.setDefaultValues(this, R.xml.pref_settings, false);
@@ -147,10 +104,6 @@ public class InitStartActivity extends Activity {
 
         dbHelper = new KcaDBHelper(getApplicationContext(), null, KCANOTIFY_DB_VERSION);
         downloader = KcaUtils.getInfoDownloader(getApplicationContext());
-        FetchConfiguration fetchConfiguration = new FetchConfiguration.Builder(getApplicationContext())
-                .setDownloadConcurrentLimit(24)
-                .build();
-        fetch = Fetch.Impl.getInstance(fetchConfiguration);
 
         KcaApiData.setDBHelper(dbHelper);
 
@@ -176,37 +129,35 @@ public class InitStartActivity extends Activity {
         setPreferences(getApplicationContext(), PREF_DATALOAD_ERROR_FLAG, false);
         loadDefaultAsset();
 
-        boolean all_passed = KcaUtils.validateResourceFiles(getApplicationContext(), dbHelper);
-        if (all_passed) {
-            setPreferences(getApplicationContext(), PREF_DATALOAD_ERROR_FLAG, false);
-            findViewById(R.id.init_layout).setOnClickListener(v -> {
-                if (!reset_flag) {
-                    startMainActivity(false);
-                    is_skipped = true;
-                }
-            });
-        }
+        validated_flag = KcaUtils.validateResourceFiles(getApplicationContext(), dbHelper);
+        setPreferences(getApplicationContext(), PREF_DATALOAD_ERROR_FLAG, false);
+        findViewById(R.id.init_layout).setOnClickListener(v -> {
+            if (validated_flag && !reset_flag) {
+                startMainActivity(false);
+                is_skipped = true;
+            }
+        });
 
-        appmessage.setText("Loading Translation Data...");
-        loadTranslationData(getApplicationContext());
+        new Thread(() -> {
+            runOnUiThread(() -> appmessage.setText("Loading Translation Data..."));
+            loadTranslationData(getApplicationContext());
 
-        appmessage.setText("Loading KanColle Game Data...");
-        int setDefaultGameDataResult = KcaUtils.setDefaultGameData(getApplicationContext(), dbHelper);
-        if (setDefaultGameDataResult != 1) {
-            Toast.makeText(this, "error loading game data", Toast.LENGTH_LONG).show();
-        }
+            runOnUiThread(() -> appmessage.setText("Loading KanColle Game Data..."));
+            int setDefaultGameDataResult = KcaUtils.setDefaultGameData(getApplicationContext(), dbHelper);
+            if (setDefaultGameDataResult != 1) {
+                runOnUiThread(() -> Toast.makeText(getApplicationContext(), "error loading game data", Toast.LENGTH_LONG).show());
+            }
 
-        appmessage.setText("Checking External Filter...");
-        boolean allow_ext = KcaUtils.getBooleanPreferences(getApplicationContext(), PREF_ALLOW_EXTFILTER);
-        List<String> result = KcaVpnData.setExternalFilter(allow_ext);
+            runOnUiThread(() -> appmessage.setText("Checking External Filter..."));
+            boolean allow_ext = KcaUtils.getBooleanPreferences(getApplicationContext(), PREF_ALLOW_EXTFILTER);
+            List<String> result = KcaVpnData.setExternalFilter(allow_ext);
 
-        if (!KcaUtils.checkOnline(getApplicationContext()) || !getBooleanPreferences(getApplicationContext(), PREF_CHECK_UPDATE_START)) {
-            startMainActivity(true);
-        } else {
-            appmessage.setText("Checking Updates...");
-            handler = new Handler();
-            Thread t = new Thread(() -> {
+            // main update check
+            if (!KcaUtils.checkOnline(getApplicationContext()) || !getBooleanPreferences(getApplicationContext(), PREF_CHECK_UPDATE_START)) {
+                startMainActivity(true);
+            } else {
                 if (is_skipped) return;
+                runOnUiThread(() -> appmessage.setText("Checking Updates..."));
                 String currentVersion = BuildConfig.VERSION_NAME;
 
                 final Call<String> rv_data = downloader.getRecentVersion();
@@ -232,56 +183,59 @@ public class InitStartActivity extends Activity {
                     String recentVersion = response_data.get("version").getAsString();
                     if (!compareVersion(currentVersion, recentVersion)) { // True if latest
                         JsonObject data = response_data;
-                        AlertDialog.Builder alertDialog = new AlertDialog.Builder(InitStartActivity.this);
-                        alertDialog.setCancelable(false);
-                        alertDialog.setMessage(KcaUtils.format(getStringWithLocale(R.string.sa_checkupdate_hasupdate), recentVersion));
-                        alertDialog.setPositiveButton(getStringWithLocale(R.string.dialog_ok),
-                                (dialog, which) -> {
-                                    String downloadUrl = getStringPreferences(getApplicationContext(), PREF_APK_DOWNLOAD_SITE);
-                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(downloadUrl));
-                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                    if (intent.resolveActivity(getPackageManager()) != null) {
-                                        startActivity(intent);
-                                    } else if (downloadUrl.contains(getStringWithLocale(R.string.app_download_link_playstore))) {
-                                        Toast.makeText(getApplicationContext(), "Google Play Store not found", Toast.LENGTH_LONG).show();
-                                        AlertDialog.Builder apkDownloadPathDialog = new AlertDialog.Builder(InitStartActivity.this);
-                                        apkDownloadPathDialog.setIcon(R.mipmap.ic_launcher);
-                                        apkDownloadPathDialog.setTitle(getStringWithLocale(R.string.setting_menu_app_title_down));
-                                        apkDownloadPathDialog.setCancelable(true);
-                                        apkDownloadPathDialog.setItems(R.array.downloadSiteOptionWithoutPlayStore, new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialogInterface, int i) {
-                                                String[] path_value = getResources().getStringArray(R.array.downloadSiteOptionWithoutPlayStoreValue);
-                                                setPreferences(getApplicationContext(), PREF_APK_DOWNLOAD_SITE, path_value[i]);
-                                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(path_value[i]));
-                                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                                startActivity(intent);
-                                            }
-                                        });
-                                        apkDownloadPathDialog.show();
-                                    }
-                                });
-                        alertDialog.setNegativeButton(getStringWithLocale(R.string.dialog_cancel),
-                                (dialog, which) -> {
-                                    dataCheck(data);
-                                });
-
-                        handler.post(() -> {
-                            if (!is_skipped) {
+                        if (!is_skipped) {
+                        runOnUiThread(() -> {
+                                AlertDialog.Builder alertDialog = getUpdateAlertDialog(recentVersion, data);
                                 AlertDialog alert = alertDialog.create();
                                 alert.setIcon(R.mipmap.ic_launcher);
                                 alert.setTitle(
                                         getStringWithLocale(R.string.sa_checkupdate_dialogtitle));
                                 alert.show();
-                            }
-                        });
+                            });
+                        }
                     } else {
                         dataCheck(response_data);
                     }
                 }
-            });
-            t.start();
-        }
+            }
+        }).start();
+    }
+
+    private AlertDialog.Builder getUpdateAlertDialog(String recentVersion, JsonObject data) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(InitStartActivity.this);
+        alertDialog.setCancelable(false);
+        alertDialog.setMessage(KcaUtils.format(getStringWithLocale(R.string.sa_checkupdate_hasupdate), recentVersion));
+        alertDialog.setPositiveButton(getStringWithLocale(R.string.dialog_ok),
+                (dialog, which) -> {
+                    String downloadUrl = getStringPreferences(getApplicationContext(), PREF_APK_DOWNLOAD_SITE);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(downloadUrl));
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    } else if (downloadUrl.contains(getStringWithLocale(R.string.app_download_link_playstore))) {
+                        Toast.makeText(getApplicationContext(), "Google Play Store not found", Toast.LENGTH_LONG).show();
+                        AlertDialog.Builder apkDownloadPathDialog = new AlertDialog.Builder(InitStartActivity.this);
+                        apkDownloadPathDialog.setIcon(R.mipmap.ic_launcher);
+                        apkDownloadPathDialog.setTitle(getStringWithLocale(R.string.setting_menu_app_title_down));
+                        apkDownloadPathDialog.setCancelable(true);
+                        apkDownloadPathDialog.setItems(R.array.downloadSiteOptionWithoutPlayStore, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                String[] path_value = getResources().getStringArray(R.array.downloadSiteOptionWithoutPlayStoreValue);
+                                setPreferences(getApplicationContext(), PREF_APK_DOWNLOAD_SITE, path_value[i]);
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(path_value[i]));
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        });
+                        apkDownloadPathDialog.show();
+                    }
+                });
+        alertDialog.setNegativeButton(getStringWithLocale(R.string.dialog_cancel),
+                (dialog, which) -> {
+                    dataCheck(data);
+                });
+        return alertDialog;
     }
 
     private void dataCheck(JsonObject response_data) {
@@ -329,19 +283,23 @@ public class InitStartActivity extends Activity {
 
     private void startMainActivity(boolean transition) {
         if (!is_destroyed) {
-            Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(mainIntent);
-            finish();
+            runOnUiThread(() -> {
+                Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            });
         }
         //if(transition) overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 
     private void startUpdateActivity() {
         if (!is_destroyed) {
-            Intent updateIntent = new Intent(getApplicationContext(), UpdateCheckActivity.class);
-            updateIntent.putExtra("main_flag", true);
-            startActivity(updateIntent);
-            finish();
+            runOnUiThread(() -> {
+                Intent updateIntent = new Intent(getApplicationContext(), UpdateCheckActivity.class);
+                updateIntent.putExtra("main_flag", true);
+                startActivity(updateIntent);
+                finish();
+            });
         }
     }
 
@@ -407,9 +365,6 @@ public class InitStartActivity extends Activity {
         Log.e("KCA-DA", "destroy");
         is_destroyed = true;
         dbHelper.close();
-        if (mProgressDialog != null && mProgressDialog.isShowing()) {
-            mProgressDialog.dismiss();
-        }
         super.onDestroy();
     }
 
