8ecd55904438ef2a705f83634e1872c3f9a88036
antest1 <alias_n@live.jp>
2020-01-19 18:47:00 +0900

    condition empty check on quest track

--- app/src/main/java/com/antest1/kcanotify/KcaQuestTracker.java
+++ app/src/main/java/com/antest1/kcanotify/KcaQuestTracker.java
@@ -863,9 +863,10 @@ public class KcaQuestTracker extends SQLiteOpenHelper {
                 JsonArray cond = questTrackInfo.getAsJsonArray("cond");
                 int type = questTrackInfo.get("type").getAsInt();
                 for (int i = 0; i < cond.size(); i++) {
+                    if (cond_value[i] == null || cond_value[i].equals("")) continue;
                     if(Integer.parseInt(cond_value[i]) >= Integer.parseInt(cond.get(i).getAsString())) {
                         if (!checkQuestValid(type, Integer.parseInt(key), time)) {
-                            db.delete(qt_table_name, "KEY=?", new String[]{String.valueOf(key)});
+                            db.delete(qt_table_name, "KEY=?", new String[]{key});
                         } else {
                             counter += 1;
                         }
