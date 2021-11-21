e0678a8e7dfa103b041b93bfc6b9ee9671d8a8e7
rassvet2 <dekabrist19480705@gmail.com>
2021-11-21 19:00:08 +0900

    Implementation for Akashi's repair added to KcaDocking

--- app/src/main/java/com/antest1/kcanotify/KcaDocking.java
+++ app/src/main/java/com/antest1/kcanotify/KcaDocking.java
@@ -38,14 +38,14 @@ public class KcaDocking {
     }
     public static int getDockingTime(int hp_loss, int level, int stype) {
         if (hp_loss > 0) {
-                int repair_time = 0;
-                double multiplier = getMultiplier(stype);
-                if (level <= 11) {
-                    repair_time = 30 + (int) (hp_loss * (level * 10) * multiplier);
-                } else {
-                    repair_time = 30 + (int) (hp_loss * ((level * 5) + Math.floor(Math.sqrt(level - 11)) * 10 + 50) * multiplier);
-                }
-                return repair_time;
+            int repair_time = 0;
+            double multiplier = getMultiplier(stype);
+            if (level <= 11) {
+                repair_time = 30 + (int) (hp_loss * (level * 10) * multiplier);
+            } else {
+                repair_time = 30 + (int) (hp_loss * ((level * 5) + Math.floor(Math.sqrt(level - 11)) * 10 + 50) * multiplier);
+            }
+            return repair_time;
         } else {
             return 0;
         }
@@ -55,5 +55,45 @@ public class KcaDocking {
         else if (type == STYPE_CA || type == STYPE_CAV || type == STYPE_FBB || type == STYPE_CVL || type == STYPE_AS ) return 1.5;
         else if (type == STYPE_SS || type == STYPE_DE ) return 0.5;
         else return 1.0;
+    }
+
+    private static double getRepair1HpTime(int level, int stype) {
+        double mult = getMultiplier(stype);
+        if (level <= 11) {
+            return level * 10 * mult;
+        } else {
+            double amp = Math.floor(Math.sqrt(level - 11)) * 10 + 50;
+            return (level * 5 + amp) * mult;
+        }
+    }
+
+    /**
+     *
+     * @param level level of the ship to be repaired
+     * @param stype ship type of the ship
+     * @param elapsed elapsed seconds
+     * @return
+     */
+    public static int getRepairedHp(int level, int stype, int elapsed) {
+        return (int) Math.floor(elapsed / getRepair1HpTime(level, stype));
+
+        // 10秒たった
+        // 3.1秒で1回復
+        // maybe = 10 / 3.1 = 3.22 ~~ 3すでに回復しているはず
+        // でもceil(3.1 * 3) = 10
+
+        // 極端な例の検証不足
+        // TODO: exhaustive check is needed
+    }
+
+    /**
+     *
+     * @param level level of the ship to be repaired
+     * @param stype ship type of the ship
+     * @param elapsed elapsed seconds
+     * @return
+     */
+    public static int getNextRepair(int level, int stype, int elapsed) {
+        return (int) Math.ceil(getRepair1HpTime(level, stype) * (getRepairedHp(level, stype, elapsed) + 1));
     }
 }
