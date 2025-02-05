192d076fc224f75b8b6bc5346d25f55bb4f6f2bb
antest1 <kcanotify@gmail.com>
2025-02-05 12:19:15 +0900

    implement automation receiver for new vpnservice

--- app/src/main/java/com/antest1/kcanotify/KcaAutomationReceiver.java
+++ app/src/main/java/com/antest1/kcanotify/KcaAutomationReceiver.java
@@ -4,20 +4,20 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.net.VpnService;
-import android.os.Handler;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.widget.Toast;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import static com.antest1.kcanotify.KcaConstants.BROADCAST_ACTION;
-import static com.antest1.kcanotify.KcaConstants.CONTENT_URI;
+import static com.antest1.kcanotify.KcaConstants.PREF_SNIFFER_MODE;
+import static com.antest1.kcanotify.KcaConstants.PREF_USE_TLS_DECRYPTION;
 import static com.antest1.kcanotify.KcaConstants.PREF_VPN_ENABLED;
-import static com.antest1.kcanotify.KcaConstants.VPN_STOP_REASON;
+import static com.antest1.kcanotify.KcaConstants.SNIFFER_PASSIVE;
+import static com.antest1.kcanotify.KcaUtils.getBooleanPreferences;
+import static com.antest1.kcanotify.KcaUtils.getStringPreferences;
+
+import com.antest1.kcanotify.remote_capture.CaptureHelper;
+import com.antest1.kcanotify.remote_capture.CaptureService;
+import com.antest1.kcanotify.remote_capture.MitmAddon;
+import com.antest1.kcanotify.remote_capture.model.CaptureSettings;
 
 public class KcaAutomationReceiver extends BroadcastReceiver {
     public static final String SNIFFER_ON_ACTION = "sniffer_on";
@@ -28,27 +28,53 @@ public class KcaAutomationReceiver extends BroadcastReceiver {
         String action = intent.getAction();
         if (action != null) {
             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+            int sniffer_mode = Integer.parseInt(getStringPreferences(context, PREF_SNIFFER_MODE));
+            if (sniffer_mode == SNIFFER_PASSIVE) return; // do nothing when passive mode
             switch (action) {
-                /*
                 case SNIFFER_ON_ACTION:
-                    try {
-                        VpnService.prepare(context);
+                    if (startCapture(context))
                         prefs.edit().putBoolean(PREF_VPN_ENABLED, true).apply();
-                        KcaVpnService.start("prepared", context);
-                    } catch (Throwable ex) {
-                        // Prepare failed
-                        Log.e("KCA", ex.toString() + "\n" + Log.getStackTraceString(ex));
-                    }
                     break;
-
                 case SNIFFER_OFF_ACTION:
-                    KcaVpnService.stop(VPN_STOP_REASON, context);
+                    stopCapture();
                     prefs.edit().putBoolean(PREF_VPN_ENABLED, false).apply();
                     break;
-                */
                 default:
                     break;
             }
         }
+    }
+
+    public boolean startCapture(Context context) {
+        if (CaptureService.isServiceActive()) return true;
+
+        if (getBooleanPreferences(context, PREF_USE_TLS_DECRYPTION)) {
+            MitmAddon.setCAInstallationSkipped(context, false);
+            if (MitmAddon.needsSetup(context)) {
+                Toast.makeText(context, "mitm_needs_setup", Toast.LENGTH_LONG).show();
+                return false;
+            }
+
+            if (!MitmAddon.getNewVersionAvailable(context).isEmpty()) {
+                Toast.makeText(context, context.getString(R.string.mitm_addon_update_available), Toast.LENGTH_LONG).show();
+                return false;
+            }
+        }
+
+        doStartCaptureService(context);
+        return true;
+    }
+
+    public void stopCapture() {
+        if (!CaptureService.isServiceActive()) return;
+        CaptureService.stopService();
+    }
+
+    private void doStartCaptureService(Context context) {
+        // appStateStarting();
+        CaptureHelper mCapHelper = new CaptureHelper(context);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        CaptureSettings settings = new CaptureSettings(context, prefs);
+        mCapHelper.startCapture(settings);
     }
 }
